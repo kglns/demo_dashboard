ac4f62443c8068bbf7588d1011982f84be083db7
Kaung L <kaung.lin@gmail.com>
2022-11-27 11:27:40 +0700

    ProfileCards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+// self explanatory - import custom css
+import './App.css'
+
+import ProfileCards from './SampleList.js'
+
 const App = () => {
-  return (
-    <div>App</div>
-  )
+    return ProfileCards()
 }
 
 export default App
