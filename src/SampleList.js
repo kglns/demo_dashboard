39e85a967b23ebc8b0d7dfeaf579a5af2bbe283b
Kaung L <kaung.lin@gmail.com>
2022-11-27 12:20:36 +0700

    Add tailwind grid

--- src/SampleList.js
+++ src/SampleList.js
@@ -1,3 +1,5 @@
+import { row } from "@syncfusion/ej2-react-grids"
+
 const people = [
     {
       name: 'Calvin Hawkins',
@@ -17,22 +19,99 @@ const people = [
       image:
         'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
+    {
+      name: 'Calvin Hawkins',
+      email: 'calvin.hawkins@example.com',
+      image:
+        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      name: 'Kristen Ramos',
+      email: 'kristen.ramos@example.com',
+      image:
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      name: 'Ted Fox',
+      email: 'ted.fox@example.com',
+      image:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      name: 'Calvin Hawkins',
+      email: 'calvin.hawkins@example.com',
+      image:
+        'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      name: 'Kristen Ramos',
+      email: 'kristen.ramos@example.com',
+      image:
+        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+      name: 'Ted Fox',
+      email: 'ted.fox@example.com',
+      image:
+        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
   ]
-  
-  export default function ProfileCards() {
-    return (
-      <ul className="pt-6">
-        {people.map((person) => (
-          <div class="max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
-          <li key={person.email} className="py-4 flex">
-            <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-            <div className="ml-3">
-              <p className="text-sm font-medium text-gray-900">{person.name}</p>
-              <p className="text-sm text-gray-500">{person.email}</p>
-            </div>
-          </li>
-          </div>
-        ))}
-      </ul>
-    )
+
+const table = [ people, people, people ]
+
+const peopleAndDogs = [
+  { 
+    name: 'chris',
+    pets: [
+      { name: 'bella', type: 'dog' },
+      { name: 'cocoa', type: 'dog' }
+    ]
+  },
+  { 
+    name: 'nick',
+    pets: [
+      { name: 'hilo', type: 'cat' },
+      { name: 'polly', type: 'cat' }
+    ]
   }
+];
+
+export default function NestedProfileCards() {
+  return (
+    <div class="grid grid-rows-3 grid-flow-col gap-0.1">
+  {people.map((person) => (
+        <div className="pt-1 pl-1 pr-1">
+          <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <li key={person.email} className="py-4 flex">
+              <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+              <div className="ml-3">
+                <p className="text-sm font-medium text-gray-900">{person.name}</p>
+                <p className="text-sm text-gray-500">{person.email}</p>
+              </div>
+            </li>
+          </div>
+        </div>
+      ))}
+</div>
+  )
+}
+  
+function SimpleProfileCards(people) {
+  return (
+    <ul>
+      {people.map((person) => (
+        <div className="pt-3">
+          <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <li key={person.email} className="py-4 flex">
+              <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
+              <div className="ml-3">
+                <p className="text-sm font-medium text-gray-900">{person.name}</p>
+                <p className="text-sm text-gray-500">{person.email}</p>
+              </div>
+            </li>
+          </div>
+        </div>
+      ))}
+    </ul>
+  )
+}
